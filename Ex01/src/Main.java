ea23aeb451c0d880959cbb483845f3f00d0b9b3c
ephammer <kidon.613@gmail.com>
2017-03-21 19:44:41 +0200

    First Part minus test

---
+++ Ex01/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kid0n on 21/03/2017.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // TODO: Create instances of dfierent classes for test purposes
+    }
+}
